df97906086c20853d0a5201498aa5928e4383331
Tatjana Nebel <tatjana.nebel@outlook.com>
2016-07-04 16:28:21 +0200

    changes to demo

--- timetracking/app/controllers/site_controller.rb
+++ timetracking/app/controllers/site_controller.rb
@@ -1,8 +1,26 @@
 class SiteController < ApplicationController
+  layout 'admin'
   def home
     render 'home'
   end
   def contact
-    render 'contact'
+    render 'contact', layout: 'admin'
+  end
+
+  def say_name
+    @name = params[:name]
+    render 'say_name'
+  end
+
+  def calculator
+    render 'calculator'
+  end
+
+  def calculate
+    @num1 = params[:first_num].to_f
+    @num2 = params[:second_num].to_f
+
+    @result = @num1 + @num2
+    render 'calculate'
   end
 end
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get '/', to: 'site#home'
   get '/contact', to: 'site#contact'
+  get '/say_name/:name', to: 'site#name'
+    get '/calculator', to: 'site#calculator'
+  get '/calculate', to: 'site#calculate'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
